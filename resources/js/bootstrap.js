210fbdd738c0abee582bbf1b0eea3df3e7b09d54
codewithashraf <ar1246261@gmail.com>
2025-05-22 17:42:26 +0500

    add axios configuration: https setting ki hai

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL; // ✅ Yeh line add karo
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import './echo';
